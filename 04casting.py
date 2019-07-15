935853aa399e272e766bf22e004e3c6c8bbb08cf
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-15 12:14:39 +0100

    did some casting

---
+++ 04casting.py
@@ -0,0 +1,20 @@
+# Casting is when you change the data type of an object
+# we can cast strings into intergers  or intergers into strings
+
+
+
+
+# Casting sting class to interger class
+
+number_string ='10'
+print(type(number_string))
+# now changing to interger using int()
+number_int= int(number_string)
+print(type(number_int))
+# Cating interger class to string
+print("now let us do the same but for strings into intergers")
+number_int2 = 11
+#change to string using str()
+
+number_int2_str = str(number_int2)
+print(type(number_int2_str))
