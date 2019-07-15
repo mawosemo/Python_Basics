1fe7480372e71b3e403422e2e0f6eb5276c0ba9e
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-15 12:01:48 +0100

    did some intergers

---
+++ 03Datatypesints.py
@@ -0,0 +1,59 @@
+# Numericals - intetrger, big integer, complex numbers
+
+# Wnumber
+print(13)
+print(type(13)) # Not the same as print (type('13'))-->string!
+
+#Floats are composite numbers
+print(13.1)
+print(type(13.1))
+
+
+#long and complex
+# Less used check sytax when needed
+
+#Opperators
+#Add
+
+num_a=10
+num_b= 7.3
+print(num_a + num_b)
+print(num_a - num_b)
+print(num_b*10)
+print(num_b/2)
+
+
+#Modulus -->>> checks for remainders
+print(10 %  3)
+
+# check if even
+print(12 %  2)
+
+print(18 %  3)
+
+#Logical operators
+# use to evaluate two sides a creates a boolean
+#check if equal
+
+a = 16
+b = 17
+
+print(a==b)
+print(17 == b)
+
+# checking if a valu is smaller or bigger
+print(a > b)
+print(a > 9)
+print(-----)
+print(b < a)
+print(b < 15)
+# check if not something
+print(a != 9)
+
+# Increment
+counter = 10
+print(counter)
+counter += 1
+print(counter)
+counter += 12
+print(counter)
