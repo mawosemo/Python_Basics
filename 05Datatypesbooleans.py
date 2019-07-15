59258b2156dfed8045d70548c31eeb55bc559101
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-15 14:38:25 +0100

    did some boolean

---
+++ 05Datatypesbooleans.py
@@ -0,0 +1,48 @@
+
+Booleans
+Booleans is a data type that is either true or false
+
+#syNT
+
+
+
+
+
+#when >=/<= equate / evaulate something we get a boolean as a response
+#Logical Operator return boolean
+#==/!= / <>/ >=/<=
+
+
+
+weather='rainy'
+
+print(weather == 'sunny')
+print(weather == 'rainy't)
+
+
+#logical AND &  OR
+# evaulate two side and both sides have to be the same for it to return true
+
+print(True and True)
+print(True and  False)
+
+print('testing logical and: ')
+print(weather== 'rainy' and weather =='')
+
+print('Location in code 2')
+text ='Hello World!'
+print(text[0]startswith('0H'))
+print(text.startswith('H'))
+print(text.startswith('h'))
+print(text.startswith('Testing .endswith(arg) ')
+print(text.startswith('!'))
+print(text.startswith('?'))
+
+
+#Booleans and numbers
+print("printing bool values of numbers")
+print(bool(0))
+print(bool(13))
+print(bool(1))
+print(bool (-1))
+print(bool (3.))
