c5ff24361a38c9b60cc3100920d4941f8f636b80
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-15 10:08:36 +0100

    variables

---
+++ 01variables.py
@@ -0,0 +1,11 @@
+#Variables in Python
+
+my_variable = 'Hello there'
+
+print(my_variable)# printed bythe console
+
+# has a name
+# stores things inside
+my_variable="I'm new content being reassigined to my_variable"
+
+print(my_variable36)
