7b6c515e4f54677dfd8fc75ec9f32a6ca6c6c34f
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-15 13:58:08 +0100

    did some funny details

---
+++ user_details_funny_excercise.py
@@ -0,0 +1,20 @@
+# this is where i will o my excersize as filipe told
+
+
+# name
+print('WHATS YOUR NAME FOOOOOL!!!!')
+user_name =input('please input here -->')
+# most important date
+print('GIVE Me A DATE FOOOOOL!!!!')
+important_date ='please input here -->'
+important_date_int = int(input (important_date))
+print(type)
+
+print('WHY THAT DATE FOOOL !!!!')
+Laugh = input('please give your reasoning -->' )
+print('CHOOSE A POKEMON!!!!')
+pokemon = input(' CHOOSE SQUIRTLEEEEEEE !!!! -->' )
+# reason for most important date
+# funny fact
+# favourite pokemon
+#input( )
