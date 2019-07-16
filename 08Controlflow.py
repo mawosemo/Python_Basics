251b182a65064b8b12ddf33456105cfe7fb4451d
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-16 10:58:20 +0100

    started control flows

---
+++ 08Controlflow.py
@@ -0,0 +1,33 @@
+#Control Flow - If statements
+# syntax - INDENTATION MATTERS
+# if <conditon>:
+# #     Block of code
+# else:
+#     Block of code
+
+#
+# age = 16
+#
+# if age >= 70 :
+#     print('your OLD, YOUR NOT THE STALLION YOU ONCE WAS, SIT DOWN!!!!!!!!!!!!! ')
+# elif age >= 18:
+#     print('your age is above 18, you can vote! :D and go to prison ')
+#
+# elif age >= 16:w
+#     print('you can buy the lottery and probably go to prison but your record might be cleaned.... depending on what you do')
+# else:w
+#     print('you are under the age to vote'
+
+
+
+
+
+
+weather = input ("what's the weather like").lower().strip()
+if weather == 'Rainy' or weather =='stormy':
+    print("Ain't no sunshine when she's gone")
+if weather == 'Sunny' :
+    print("Don't blame on the sunshine")
+else:
+    print(" DON'T STOP BELIEVING" )
+
