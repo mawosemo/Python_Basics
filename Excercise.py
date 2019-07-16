b00a961a29e1da81e8bcec806f386f3c00670777
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-16 09:38:40 +0100

    pokemon assemble

--- Excercise.py
+++ Excercise.py
@@ -1,19 +1,20 @@
 Sparta_clients =  ['ASOS',' BBC',' MEN IN BLACK ','Galactic  Empire','KGB', 'Rebel Alliance','The Kingsmen','S.H.I.E.L.D', 'Avengers']
-
-#y =
- #print(y)
-#     #let us use interpolation
-A = f" {Sparta_clients[0]}, is a cool client of sparta "
-print(A)
-B = f" {Sparta_clients[1]}, is a cool client of sparta "
-print(B)
-C = f" {Sparta_clients[2]}, is a cool client of sparta "
-print(C)
-D = f" {Sparta_clients[3]}, is a cool client of sparta "
-print(D)
-E = f" {Sparta_clients[4]}, is a cool client of sparta "
-print(E)
-
+#
+# #y =
+#  #print(y)
+# #     #let us use interpolation
+# A = f" {Sparta_clients[0]}, is a cool client of sparta "
+# print(A)
+# B = f" {Sparta_clients[1]}, is a cool client of sparta "
+# print(B)
+# C = f" {Sparta_clients[2]}, is a cool client of sparta "
+# print(C)
+# D = f" {Sparta_clients[3]}, is a cool client of sparta "
+# print(D)
+# E = f" {Sparta_clients[4]}, is a cool client of sparta "
+# print(E)
+G = Sparta_clients[5] + " is a cool client of alliance"
+print(G)
 first_name = input('what is your name?')
 last_name = input('No your full name, ADD YOUR SURNAME FOOL?')
 full_name = first_name+ ''+ last_name
@@ -26,10 +27,10 @@ elif hp <= 30:
 Pika={
     'hero': full_name,
   'beginning': ' In the beginning there was hero called ',
-  'middle':'he was 17 years old, he had the following pokemon pikachu,charmander,Squirtle,Mewtwo,and was always grumpy',
+  'middle':'is 17.4 years old, he had the following pokemon pikachu,charmander,Squirtle,Mewtwo,and was always grumpy',
   'end':'he died at age 17.5',
 }
 #Access infomation using the keys
 print(Pika['beginning']+ Pika['hero'])
-print(Pika['middle'])
+print(Pika['hero']+Pika['middle'])
 print(Pika['hero']+Pika['end'])
