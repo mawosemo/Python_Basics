cb6e9efdb81c90a0382309f8688ddd4e42e4268c
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-07-17 09:29:16 +0100

    calculator 2.0

--- Calculator.py
+++ Calculator.py
@@ -27,20 +27,23 @@ def divide(value1, value2):
 def area(value1):
     value3 = value1*value1*3.14
     return float(value3*2.54)
+while True:
+    value1 = int(input('please insert a number: '))
+    function = input('insert function ')
+    value2 = int(input('please insert another number: '))
 
-value1 = int(input('please insert a number: '))
-function = input('insert function ')
-value2 = int(input('please insert another number: '))
 
-if function == 'area':
-    print('the area of a circle is ', area(value1))
-elif function == '-':
-    print('the following subtract is ', subtract(value1, value2))
-elif function == '*':
-    print('the following multiplication is ', multiply(value1, value2))
-elif function == '/':
-    print('the following division is ', divide(value1, value2))
-elif function == '+':
-    print('the following addition is ', add(value1, value2))
-else:
-    print('invalid response')
+    if function == 'area':
+        print('the area of a circle is ', area(value1))
+    elif function == '-':
+        print('the following subtract is ', subtract(value1, value2))
+    elif function == '*':
+        print('the following multiplication is ', multiply(value1, value2))
+    elif function == '/':
+        print('the following division is ', divide(value1, value2))
+    elif function == '+':
+        print('the following addition is ', add(value1, value2))
+    elif function == 'end':
+        break
+    else:
+        print('invalid response')
